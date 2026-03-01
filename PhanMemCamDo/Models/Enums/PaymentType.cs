4f3da73d98338a5e4d7298b14f168cc005139767
Nguyen12345tt <thanhnguyennguyen496@gmail.com>
2026-03-01 21:11:49 +0700

    feat(PaymentType): add PaymentType enum for managing payment categories

---
+++ PhanMemCamDo/Models/Enums/PaymentType.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PhanMemCamDo.Models.Enums
+{
+    public enum PaymentType
+    {
+        [Display(Name = "Đóng Lãi")]
+        Interest = 1,
+
+        [Display(Name = "Trả Bớt Gốc")]
+        Principal = 2,
+
+        [Display(Name = "Chuộc Đồ (Tất Toán)")]
+        Redeem = 3,
+
+        [Display(Name = "Khác")]
+        Other = 4
+    }
+}
